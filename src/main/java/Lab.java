6fc02536534a06f3b9050cd829cb971a2584a34c
kokouvi Gbenou <kokouvigbenourevature@gmail.com>
2023-03-06 05:58:43 +0000

    Lab.instantiateANewObject() method completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+    Object oklm = new String();
+        return oklm;
     }
 }
